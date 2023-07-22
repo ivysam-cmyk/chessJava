972decc1ebf7d770f64a2a61f723241094fedbca
sam <sam@sam-g15>
2023-07-22 12:02:25 +0800

    Created board placements in 2d array

--- positions.java
+++ positions.java
@@ -1,11 +1,26 @@
+import java.util.Arrays;
 public class positions {
 	//Horizontal A-H
+	public String[] horizontal = {"A", "B", "C", "D", "E", "F", "G", "H"};
 	//Vertical 1-8
+	public String[] vertical = {"1", "2", "3", "4", "5", "6", "7", "8"};
 	//2d list?
-	public positions(){
-			
+	String[][] boardPositions = new String[8][8];
+	public void positions(){
+		//create the 2d array
+		for(int i =0; i<8; i++){
+			for(int j =0; j<8; j++){
+				//append the 2 strings
+				this.boardPositions[i][j] = horizontal[j]+vertical[i];	
+			}
+		}
+        for (String[] row : boardPositions) {
+            System.out.println(Arrays.toString(row));
+		}
 	}
+
 	public static void main(String[] args) {
-			
+		positions board = new positions();	
+		board.positions();
 	}	
 }
